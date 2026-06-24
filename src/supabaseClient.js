7519f77daa9d2ba4c43fe20a01918682f24eeac1
Murod907 <asomidinovmurodjon83@gmail.com>
2026-06-24 15:51:36 +0500

    Fix Supabase client initialization variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,10 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!url || !key) {
+if (!supabaseUrl || !supabaseAnonKey) {
   console.error(
-    "Supabase sozlamalari topilmadi. .env faylida VITE_SUPABASE_URL va VITE_SUPABASE_ANON_KEY ni to'g'ri kiritganingizga ishonch hosil qiling."
+   " https://ucdpnpqvjxtbccwiqzmb.supabase.co, sb_publishable_-5bkYBEiYj33OCikG5SQGw_Hz1MWrCX "
   );
 }
 
-export const supabase = createClient(url, key);
+export const supabase = createClient(supabseUrl, supabaseAnonKey);
