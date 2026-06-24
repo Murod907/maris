7d1d5335c51548d3150870cd3d5c4e5fd1ffd623
Murod907 <asomidinovmurodjon83@gmail.com>
2026-06-24 16:53:01 +0500

    Update Supabase client initialization
    Replaced environment variables with hardcoded Supabase URL and key.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const url = "https://ucdpnpqvjxtbccwiqzmb.supabase.co";
+const key = "sb_publishable_-5bkYBEiYj33OCikG5SQGw_Hz1MWrCX";
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error(
-   " https://ucdpnpqvjxtbccwiqzmb.supabase.co, sb_publishable_-5bkYBEiYj33OCikG5SQGw_Hz1MWrCX "
-  );
-}
-
-export const supabase = createClient(supabseUrl, supabaseAnonKey);
+export const supabase = createClient(url, key);
